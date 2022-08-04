473136750230d4afec9742ad57b56c468b345ac7
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-04 09:41:45 -0300

    att

--- espo_flix/src/App.jsx
+++ espo_flix/src/App.jsx
@@ -1,9 +1,15 @@
-  import './App.css'
+  import { Link } from 'react-router-dom'
+import './App.css'
 
 function App() {
  
   return (
     <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">EspoFLix</Link>
+        </h2>
+      </nav>
       <h2>EspoFlix</h2>
     </div>
   )
