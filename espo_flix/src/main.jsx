b27ea870bf8da046757893a4bf2458f5c841ec92
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-04 09:32:57 -0300

    att

--- espo_flix/src/main.jsx
+++ espo_flix/src/main.jsx
@@ -9,9 +9,11 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-   <Routes>
-        <Route element={<App/>}>
-        <Route path="/" element={<Home />} />
+        <Routes>
+           <Route element={<App/>}>
+             <Route path="/" element={<Home />} />
+             <Route path="movie/:id" element={<Movie />} />
+             <Route path="search" element={<Search />} />
         </Route>
    </Routes>
     </BrowserRouter>
