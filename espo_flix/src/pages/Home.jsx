ef16c67ebfafbdcdea0a850c3fd07a7609672a75
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-04 11:53:35 -0300

    att

--- espo_flix/src/pages/Home.jsx
+++ espo_flix/src/pages/Home.jsx
@@ -1,6 +1,9 @@
  import { useState, useEffect } from "react";
 
+  
+  //url da api
  const moviesURL = import.meta.env.VITE_API;
+//chave da api
  const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
