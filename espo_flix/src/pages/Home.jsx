fa523125ccfd7fc47bb5339f37e1a2e8364c9c53
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-04 12:15:37 -0300

    att

--- espo_flix/src/pages/Home.jsx
+++ espo_flix/src/pages/Home.jsx
@@ -7,20 +7,24 @@
  const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-const [topMovies, setTopMovies] = useState([])
+    const [topMovies, setTopMovies] = useState([]);
 
-const getTopRatedMovies =async (url) => {
- const res = await fetch(url)
- const data =await resp.json()
-
- console.log(data)
-};
-
-useEffect(()=> {
-
-}, [])
-
-    return <div>Home</div>;
+    const getTopRatedMovies = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setTopMovies(data.results);
+    };
+  
+    useEffect(() => {
+      const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+      console.log(topRatedUrl);
+      getTopRatedMovies(topRatedUrl);
+    }, []);
+  
+    console.log(topMovies);
+    return <div>
+        {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+    </div>;
     
 };
 
