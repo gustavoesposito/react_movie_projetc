b8d1caf565c080322c1df147bb32f577fdeef28d
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-05 10:43:08 -0300

    att

--- espo_flix/src/pages/Movie.jsx
+++ espo_flix/src/pages/Movie.jsx
@@ -1,7 +1,24 @@
   import { useEffect, useState } from "react";
  import { useParams } from "react-router-dom";
+ import{
+BsGraphUp,
+BsWallet2,
+BsHourglassSplit,
+BsDillFileEarmarkTextFill
+ } from 'react-icons/bs'
+
+import  MovieCard from  "../components/MovieCard";
+
+ import  './Movies.css'
+
+   //url da api
+   const moviesURL = import.meta.env.VITE_API;
+   //chave da api
+    const apiKey = import.meta.env.VITE_API_KEY;
 
 const Movie = () => {
+    const {id} = useParams();
+    const [movie, setMovie] = useState(null)
     return(
         <div>Movie</div>
     )
