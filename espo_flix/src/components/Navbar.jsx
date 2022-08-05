f61abced95fb1b5adc124fe4e50a4eafc388bd50
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-05 09:38:28 -0300

    att

--- espo_flix/src/components/Navbar.jsx
+++ espo_flix/src/components/Navbar.jsx
@@ -5,13 +5,16 @@ import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
 
 import  "./Navbar.css" ;
 const Navbar = () => {
+
+    const [search, setSearch] = useState("")
+
     return (
         <nav id="navbar">
         <h2>
             <Link to="/"><BiCameraMovie/>EspoFlix</Link>
         </h2>
          <form >
-             <input  type="text" placeholder="Busque um filme" />
+             <input  type="text" placeholder="Busque um filme"onChange={(e) => setSearch(e.target.value)}/>
              <button type="submit">
                 <BiSearchAlt2/>
              </button>
