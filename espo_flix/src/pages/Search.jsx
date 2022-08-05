933354f7da93565304b59314a82e716e937d4083
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-05 09:54:59 -0300

    attt

--- espo_flix/src/pages/Search.jsx
+++ espo_flix/src/pages/Search.jsx
@@ -1,4 +1,10 @@
- 
+ import { useState, useEffect,  } from "react";
+ //useSearchParams permite oegar a query string  d aURL e utiliza-la xomo desejo
+ import { useSearchParams } from "react-router-dom";
+ import MovieCard from "../components/MovieCard";
+
+ const searchURL = import.meta.env.VITE_SEARCH
+
 const Search = () => {
     return(
         <div>Search</div>
