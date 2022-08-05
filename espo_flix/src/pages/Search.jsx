b4d22a249387cec6b6141957ea6c5d91bb83ce39
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-05 09:57:41 -0300

    att

--- espo_flix/src/pages/Search.jsx
+++ espo_flix/src/pages/Search.jsx
@@ -3,8 +3,12 @@
  import { useSearchParams } from "react-router-dom";
  //componente que exibe dados do filme individualmente
  import MovieCard from "../components/MovieCard";
-
+//minhas url's
  const searchURL = import.meta.env.VITE_SEARCH
+ //minha apiKey
+ const apiKey = import.meta.env.VITE_API_KEY
+
+ import
 
 const Search = () => {
     return(
