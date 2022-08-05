72ba4998e056fe0ddfffb290626c462ee9f413fd
gustavoesposito <105567405+gustavoesposito@users.noreply.github.com>
2022-08-05 10:32:21 -0300

    att

--- espo_flix/src/pages/Movie.jsx
+++ espo_flix/src/pages/Movie.jsx
@@ -1,4 +1,6 @@
- 
+  import { useEffect, useState } from "react";
+ import { useParams } from "react-router-dom";
+
 const Movie = () => {
     return(
         <div>Movie</div>
--- espo_flix/src/pages/Search.jsx
+++ espo_flix/src/pages/Search.jsx
@@ -10,22 +10,35 @@
 
  import "./MoviesGrid.css";
 
-const Search = () => {
-const [searchParams ] = useSearchParams();
 
-const [movies, setMovies] = useState([]);
-const query = searchParams.get("q")
-    
-
-    return(
-    <div className="container">
-            <h2 className="title">Resultados para <span className="query-text"></span>{query}</h2>
-            <div className="movies-container">
-                {topMovies === 0 &&  <p>Carrgando...</p>}
-            {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
-            </div>
+ const Search = () => {
+    const [searchParams] = useSearchParams();
+  
+    const [movies, setMovies] = useState([]);
+    const query = searchParams.get("q");
+  
+    const getSearchedMovies = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    };
+  
+    useEffect(() => {
+      const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+      getSearchedMovies(searchWithQueryURL);
+    }, [query]);
+  
+    return (
+      <div className="container">
+        <h2 className="title">
+          Resultados para: <span className="query-text">{query}</span>
+        </h2>
+        <div className="movies-container">
+          {movies.length > 0 &&
+            movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
         </div>
+      </div>
     );
-}
-
-export default Search;
+  };
+  
+  export default Search;
